6015dd5ae22da725445046af1c95f30be004272a
mind_control <mind_control@mail.ru>
2020-05-05 17:06:34 +0300

    Some Mistakes Fixed

--- src/main/java/ru/netology/manager/TicketsManager.java
+++ src/main/java/ru/netology/manager/TicketsManager.java
@@ -12,20 +12,20 @@ import java.util.Arrays;
 public class TicketsManager {
     private TicketsRepository ticketsRepository;
 
-    public void add(Ticket offer) {
-        ticketsRepository.save(offer);
+    public void add(Ticket ticket) {
+        ticketsRepository.save(ticket);
     }
 
     public Ticket[] findFromTo(String from, String to) {
         Ticket[] result = new Ticket[0];
 
-        for (Ticket offer : ticketsRepository.getAll()) {
+        for (Ticket ticket : ticketsRepository.getAll()) {
             int length = result.length;
-            if (offer.getDeparture().equals(from) && offer.getArrival().equals(to)) {
+            if (ticket.getDeparture().equals(from) && ticket.getArrival().equals(to)) {
                 Ticket[] tmp = new Ticket[length + 1];
                 System.arraycopy(result, 0, tmp, 0, length);
                 int lastIndex = tmp.length - 1;
-                tmp[lastIndex] = offer;
+                tmp[lastIndex] = ticket;
                 result = tmp;
             }
             Arrays.sort(result);
@@ -36,13 +36,13 @@ public class TicketsManager {
     public Ticket[] findFrom(String from) {
         Ticket[] result = new Ticket[0];
 
-        for (Ticket offer : ticketsRepository.getAll()) {
+        for (Ticket ticket : ticketsRepository.getAll()) {
             int length = result.length;
-            if (offer.getDeparture().equals(from)) {
+            if (ticket.getDeparture().equals(from)) {
                 Ticket[] tmp = new Ticket[length + 1];
                 System.arraycopy(result, 0, tmp, 0, length);
                 int lastIndex = tmp.length - 1;
-                tmp[lastIndex] = offer;
+                tmp[lastIndex] = ticket;
                 result = tmp;
             }
             Arrays.sort(result);
